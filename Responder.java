71e86ffd39d4802c240660071db771eac3354a13
Danush <danushsoosai28@gmail.com>
2025-03-07 09:55:07 -0500

    Responder2.0

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,19 +10,40 @@
  */
 public class Responder
 {
+    private Random rand_gen;
+    private ArrayList<String> responses;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        rand_gen = new Random();
+        responses = new ArrayList<>();
+        fillResponse();
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
+     *
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int index = rand_gen.nextInt(responses.size());
+        return responses.get(index);
     }
-}
+
+        /**
+     * 22)
+     */private void fillResponse(){
+        responses.add("yes");
+        responses.add("no");
+        responses.add("who knows");
+        responses.add("i dont know");
+        responses.add("maybe");
+        responses.add("Doo doo");
+        }
+    
+    
+    }
+
